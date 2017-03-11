f3b2faf7b61a86f5e4fac64e50855def1ea67c5d
DirkWillem <developer@dirkwillem.nl>
2017-03-11 15:21:33 +0100

    Added websocket support for hosted app

--- web/src/app/core/services/dispatcher.service.ts
+++ web/src/app/core/services/dispatcher.service.ts
@@ -76,7 +76,7 @@ export class DispatcherService {
 
   constructor() {
     this.connection = new Promise<void>(resolve => {
-      this.socket = new WebSocket(`ws://localhost:5000/api/dispatcher`);
+      this.socket = new WebSocket(`ws://${window.location.host == 'localhost' ? 'localhost:5000' : window.location.host}/api/dispatcher`);
       this.socket.addEventListener("open", (e) => {
         resolve();
       });
